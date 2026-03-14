3fd17783a59581b02a7539d30ccb4c7752b2dd5d
g-akca <gok.akca66@gmail.com>
2026-03-14 21:16:30 +0300

    Add HeroSection and Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
+import logo from "/images/logo.svg";
+import hamburgerIcon from "/images/icon-hamburger.svg";
+
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center h-6">
+      <img src={logo} alt="Loopstudios logo" className="h-full" />
+
+      <button className="h-4 cursor-pointer">
+        <img src={hamburgerIcon} alt="Menu icon" className="h-full" />
+      </button>
     </header>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,8 +2,12 @@ import Header from "./Header.jsx";
 
 function HeroSection() {
   return (
-    <div>
+    <div className="py-10 px-6 h-162.5 flex flex-col gap-40.75 bg-[url('/images/mobile/image-hero.jpg')] bg-cover bg-no-repeat">
       <Header />
+
+      <div className="border-2 border-white py-4 px-6">
+        <p className="font-josefin text-[40px] font-light text-white uppercase leading-[100%]">Immersive experiences that deliver</p>
+      </div>
     </div>
   )
 }
