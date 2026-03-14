09a22c2a20468e030964741169b9785cdc1cdeea
g-akca <gok.akca66@gmail.com>
2026-03-15 02:33:14 +0300

    Finish CreationsSection structure and creations list

---
+++ src/components/CreationsItem.jsx
@@ -0,0 +1,8 @@
+function CreationsItem({ title, imgUrl }) {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default CreationsItem;
--- src/components/CreationsSection.jsx
+++ src/components/CreationsSection.jsx
@@ -1,7 +1,31 @@
+import CreationsItem from "./CreationsItem.jsx";
+
 function CreationsSection() {
+  const creations = [
+    { title: "Deep earth", imgUrl: "image-deep-earth.jpg" },
+    { title: "Night arcade", imgUrl: "image-night-arcade.jpg" },
+    { title: "Soccer team VR", imgUrl: "image-soccer-team.jpg" },
+    { title: "The grid", imgUrl: "image-grid.jpg" },
+    { title: "From up above VR", imgUrl: "image-from-above.jpg" },
+    { title: "Pocket borealis", imgUrl: "image-pocket-borealis.jpg" },
+    { title: "The curiosity", imgUrl: "image-curiosity.jpg" },
+    { title: "Make it fisheye", imgUrl: "image-fisheye.jpg" },
+  ];
+
   return (
-    <>
-    </>
+    <section className="flex flex-col items-center gap-8">
+      <div>
+        <h2 className="uppercase font-josefin font-light text-[32px] leading-[100%] mb-12">Our creations</h2>
+        
+        <div className="flex flex-col gap-6">
+          {creations.map(item => (
+            <CreationsItem key={item.title} {...item} />
+          ))}
+        </div>
+      </div>
+
+      <button type="button" className="border w-39.25 h-10 uppercase text-[14px] leading-[100%] tracking-[5px]">See all</button>
+    </section>
   )
 }
 
