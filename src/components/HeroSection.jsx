5a65e44cc20cc22aefbb78c5a0a6726fc88a97bf
g-akca <gok.akca66@gmail.com>
2026-03-15 04:31:55 +0300

    Add tablet responsive HeroSection styles

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,11 +2,18 @@ import Header from "./Header.jsx";
 
 function HeroSection() {
   return (
-    <div className="py-10 px-6 h-162.5 flex flex-col gap-40.75 bg-[url('/images/mobile/image-hero.jpg')] bg-cover bg-no-repeat">
+    <div 
+      className="
+        py-10 px-6 h-162.5 
+        flex flex-col gap-40.75 
+        bg-[url('/images/mobile/image-hero.jpg')] bg-cover bg-no-repeat bg-center
+        tablet:py-16 tablet:px-20 tablet:bg-[url('/images/desktop/image-hero.jpg')] tablet:gap-32.5
+      "
+    >
       <Header />
 
-      <div className="border-2 border-white py-4 px-6">
-        <p className="font-josefin text-[40px] font-light text-white uppercase leading-[100%]">Immersive experiences that deliver</p>
+      <div className="border-2 border-white py-4.5 px-6 tablet:p-10 tablet:pb-6">
+        <p className="font-josefin text-[40px] font-light text-white uppercase leading-[100%] tablet:text-[72px]">Immersive experiences that deliver</p>
       </div>
     </div>
   )
