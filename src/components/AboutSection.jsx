3ee9994501368d397178107bd916e2a52e017006
g-akca <gok.akca66@gmail.com>
2026-03-15 07:06:44 +0300

    Add desktop responsive AboutSection styles

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,8 +3,8 @@ import interactiveDesktop from "/images/desktop/image-interactive.jpg";
 
 function AboutSection() {
   return (
-    <section className="flex flex-col items-center max-w-220">
-      <picture className="w-full">
+    <section className="flex flex-col items-center w-full max-w-277.5 desktop:relative desktop:items-start">
+      <picture >
         <source
           srcSet={interactiveDesktop}
           media="(min-width: 768px)"
@@ -12,11 +12,17 @@ function AboutSection() {
         <img
           src={interactiveMobile}
           alt="Person wearing VR goggles"
-          className="w-full"
         />
       </picture>
 
-      <div className="pt-12 px-6 flex flex-col gap-4 text-center">
+      <div 
+        className="
+          pt-12 px-6 flex flex-col gap-4 text-center 
+          desktop:absolute desktop:bottom-0 desktop:right-0 desktop:w-135.25 
+          desktop:bg-white desktop:pt-24 desktop:pl-24 desktop:pr-0
+          desktop:text-start
+        "
+      >
         <h2 className="uppercase font-josefin font-light text-[32px] leading-[100%] tablet:text-5xl">The leader in interactive VR</h2>
         <p className="opacity-50">Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
       </div>
