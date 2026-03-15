c7875f5caf64d61fd180000342707f1eba5cafc5
g-akca <gok.akca66@gmail.com>
2026-03-15 06:44:31 +0300

    Add desktop responsive Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
   return (
     <header className="flex justify-between items-center h-6 relative z-1 tablet:h-8">
       <img src={logo} alt="Loopstudios logo" className="h-full" />
-
-      <button className="h-4 cursor-pointer">
+      
+      <button className="h-4 cursor-pointer desktop:hidden">
         <img src={hamburgerIcon} alt="Menu icon" className="h-full" />
       </button>
     </header>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,13 +8,14 @@ function HeroSection() {
         flex flex-col gap-40.75 
         bg-[url('/images/mobile/image-hero.jpg')] bg-cover bg-no-repeat bg-center
         tablet:py-16 tablet:px-20 tablet:bg-[url('/images/desktop/image-hero.jpg')] tablet:gap-32.5
+        desktop:px-41
       "
     >
       <div className="absolute inset-0 z-0 bg-black/40"></div>
 
       <Header />
 
-      <div className="border-2 border-white py-4.5 px-6 relative z-1 tablet:p-10 tablet:pb-5">
+      <div className="border-2 border-white py-4.5 px-6 max-w-100 relative z-1 tablet:p-10 tablet:pb-5 tablet:max-w-162.5">
         <p className="font-josefin text-[40px] font-light text-white uppercase leading-[100%] tablet:text-[72px]">Immersive experiences that deliver</p>
       </div>
     </div>
